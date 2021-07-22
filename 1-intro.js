b2600c4d7f9d91ce6dc9b3185d5670a1dac9fe22
Shawn McNulty <smcnulty75@gmail.com>
2021-07-21 23:27:49 -0400

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log('Small number');
+} else {
+  console.log('Large number');
+}
+
+console.log('Hello World! I made my first node app!');
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,7 @@
+// const items = ['item1', 'item2']
+module.exports.items = ['item1', 'item2'];
+const person = {
+  name: 'Bob',
+};
+
+module.exports.singlePerson = person;
